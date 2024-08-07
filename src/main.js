c7a625d86a1fd64cf4ea9069ec297bf94f73a3e6
Vitalii Sementsov <v.sementsov@ratkus.com.ua>
2024-08-07 11:32:00 +0300

    Add smooth scroll for navigation links

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
+//smooth scroll
+document.addEventListener('DOMContentLoaded', function () {
+  const links = document.querySelectorAll('a[href^="#"]');
+
+  for (let link of links) {
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      targetElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    });
+  }
+});
+
 //mobile menu
 const menuBtn = document.getElementById('menuBtn');
 const mobMenu = document.getElementById('mobMenu');
