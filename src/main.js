cb6e10cfe1a089987e718a9eddfb32d695605353
Vitalii Sementsov <v.sementsov@ratkus.com.ua>
2024-08-12 01:10:48 +0300

    add slider

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import $ from 'jquery';
+import 'slick-carousel';
+
 //smooth scroll
 document.addEventListener('DOMContentLoaded', function () {
   const links = document.querySelectorAll('a[href^="#"]');
